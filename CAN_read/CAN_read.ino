55bd261341a36e4417dc61585eceb09dd5ff1d88
nina-anast <anasnina02@gmail.com>
2023-11-08 18:45:06 +0200

    e

--- CAN_read/CAN_read.ino
+++ CAN_read/CAN_read.ino
@@ -25,6 +25,7 @@ void loop() {
 
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
     if (canMsg.can_id == 0x80001B73){
+    /*
     Serial.print(canMsg.can_id, HEX); // print ID
     Serial.print(" "); 
     Serial.print(canMsg.can_dlc, HEX); // print DLC
@@ -35,6 +36,10 @@ void loop() {
       Serial.print(" ");
     }
     */
+
+    int64_t inVolt = ((int64_t)canMsg.data[4] << 8) | ((int64_t)canMsg.data[5]);
+    Serial.print((int32_t)inVolt*0.1);
+    Serial.print(" ");
 
     Serial.println();
     delay(100);      
