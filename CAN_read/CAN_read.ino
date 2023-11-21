b0d0dbe985e87eb6603dfea0b04fa58028cb169d
vassdel <v.delhs@gmail.com>
2023-11-21 08:06:34 +0200

    added libraries folder

--- CAN_read/CAN_read.ino
+++ CAN_read/CAN_read.ino
@@ -1,62 +1,63 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
+MCP2515 mcp2515(10); // chip select pin: 10
 
-MCP2515 mcp2515(10);
-
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  
+
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // canSpeed = CAN_500KBPS
   mcp2515.setNormalMode();
-  
+
   Serial.println("------- CAN Read ----------");
   Serial.println("ID  DLC   DATA");
 }
 
-
 struct can_frame canMsg;
 
-void loop() {
-  
+void loop()
+{
 
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    if (canMsg.can_id == 0x80001B73){
-    /*
-    Serial.print(canMsg.can_id, HEX); // print ID
-    Serial.print(" "); 
-    Serial.print(canMsg.can_dlc, HEX); // print DLC
-    Serial.print(" ");
-    /*
-    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-      Serial.print(canMsg.data[i],HEX);
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) // return ERROR_FAIL/ERROR_OK
+  { 
+    '''0x80001B73 is input Voltage'''
+    if (canMsg.can_id == 0x80001B73)
+    {
+      /*
+      Serial.print(canMsg.can_id, HEX); // print ID
       Serial.print(" ");
-    }
-    */
+      Serial.print(canMsg.can_dlc, HEX); // print DLC
+      Serial.print(" ");
+      /*
+      for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+        Serial.print(canMsg.data[i],HEX);
+        Serial.print(" ");
+      }
+      */
 
-    int64_t inVolt = ((int64_t)canMsg.data[4] << 8) | ((int64_t)canMsg.data[5]);
-    Serial.print((int32_t)inVolt*0.1);
-    Serial.print(" ");
-    //for (int i=0; i<100; i++){
-    while (canMsg.can_id != 0x80001073){
-    /*
-    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-      Serial.print(canMsg.data[i],HEX);
+      int64_t inVolt = ((int64_t)canMsg.data[4] << 8) | ((int64_t)canMsg.data[5]);
+      Serial.print((int32_t)inVolt * 0.1);
       Serial.print(" ");
+      // for (int i=0; i<100; i++){
+      while (canMsg.can_id != 0x80001073)
+      {
+        /*
+        for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+          Serial.print(canMsg.data[i],HEX);
+          Serial.print(" ");
+        }
+        */
+        mcp2515.readMessage(&canMsg);
+        // Serial.println(canMsg.can_id, HEX); // print ID
+      }
+      int64_t tempMosfet = ((int64_t)canMsg.data[0] << 8) | ((int64_t)canMsg.data[1]);
+      Serial.print((int32_t)tempMosfet * 0.1);
+      Serial.print(" ");
+      //}
+      Serial.println();
+      delay(100);
     }
-    */
-    mcp2515.readMessage(&canMsg);
-    //Serial.println(canMsg.can_id, HEX); // print ID
-    } 
-    int64_t tempMosfet = ((int64_t)canMsg.data[0] << 8) | ((int64_t)canMsg.data[1]);
-    Serial.print((int32_t)tempMosfet*0.1);
-    Serial.print(" ");
-    //}
-    Serial.println();
-    delay(100);      
   }
-  }
-  
 }
